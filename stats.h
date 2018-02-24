67246aade15cf2b2c3f6929e7c52ad5290b9bb6b
Carl Dawson <carl@carlsdawson.com>
2018-02-23 17:37:01 -0800

    Creating Initial Coursera Repository

---
+++ stats.h
@@ -0,0 +1,39 @@
+/******************************************************************************
+ * Copyright (C) 2018 by Carl Dawson
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Carl Dawson is not liable for any misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file <stats.h> 
+ * @brief <Header file for assignment C1M1 - stats >
+ *
+ * <Implements various rudimentary functions for exploring numerical data>
+ *
+ * @author <Carl Dawson>
+ * @date <23 February 2018>
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+
+#endif /* __STATS_H__ */
