34ae383c6931f03accbffa86256b59cb06a6bca9
Carl Dawson <carl@carlsdawson.com>
2018-03-06 10:29:49 -0800

    implemented stats/print functions. changed mean and median to float.

--- stats.h
+++ stats.h
@@ -8,32 +8,102 @@
  *
  *****************************************************************************/
 /**
- * @file <stats.h> 
- * @brief <Header file for assignment C1M1 - stats >
+ * @file stats.h
+ * @brief Header file for assignment C1M1 - stats
  *
- * <Implements various rudimentary functions for exploring numerical data>
+ * Implements various rudimentary functions for exploring numerical data
  *
- * @author <Carl Dawson>
- * @date <23 February 2018>
+ * @author Carl Dawson
+ * @date 23 February 2018
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+/**
+ * @brief Prints statistics of an array
+ *
+ * A function that prints the statistics of an array including 
+ * minimum, maximum, mean and median. 
+ *
+ * @param data the array to be analysed 
+ * 
+ * @return void 
+ */
+void print_statistics(unsigned char * data);
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Prints an array
  *
- * <Add Extended Description Here>
+ * Given an array of data and a length, prints the array to the screen
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * @param data the array
+ * @param length the length of the array 'data'
+ * 
+ * @return void 
  */
+void print_array(unsigned char * data, unsigned int length);
 
+/**
+ * @brief returns the median value
+ *
+ * Given an array of data and a length, returns the median value
+ *
+ * @param data the array to be analysed
+ * @param length the length of the array 'data'
+ * 
+ * @return the median value of 'data'
+ */
+float find_median(unsigned char * data, unsigned int length);
+
+/**
+ * @brief returns the mean
+ *
+ * Given an array of data and a length, returns the mean
+ *
+ * @param data the array to be analysed
+ * @param length the length of the array 'data'
+ * 
+ * @return the mean value of 'data'
+ */
+float find_mean(unsigned char * data, unsigned int length);
+
+/**
+ * @brief returns the maximum value
+ *
+ * Given an array of data and a length, returns the maximum value
+ *
+ * @param data the array to be analysed
+ * @param length the length of the array 'data'
+ * 
+ * @return the maximum value of 'data'
+ */
+unsigned int find_maximum(unsigned char * data, unsigned int length);
+
+/**
+ * @brief returns the minimum value
+ *
+ * Given an array of data and a length, returns the minimum value
+ *
+ * @param data the array to be analysed
+ * @param length the length of the array 'data'
+ * 
+ * @return the minimum value of 'data'
+ */
+unsigned int find_minimum(unsigned char * data, unsigned int length);
+
+/**
+ * @brief sorts an array
+ *
+ * Given an array of data and a length, sorts the array from largest to
+ * smallest. (The zeroth Element is the largest value, and the last
+ * element (n-1) is the smallest value.)
+ *
+ * @param data the array to be analysed
+ * @param length the length of the array 'data'
+ * 
+ * @return a pointer to the sorted array
+ */
+unsigned char* sort_array(unsigned char * data, unsigned int length);
 
 #endif /* __STATS_H__ */
